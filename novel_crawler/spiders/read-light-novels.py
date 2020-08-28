a4ca0fc097915101e2cafe37a940d00aac7b0fcc
Marcos Barbosa <marcosbarbosa031@gmail.com>
2020-08-28 09:58:41 -0300

    Trying to get all chapters per page

--- novel_crawler/spiders/read-light-novels.py
+++ novel_crawler/spiders/read-light-novels.py
@@ -1,23 +1,34 @@
 import scrapy
+import re
+
 from novel_crawler.items import NovelChapterItem
 
 class ReadlightnovelsSpider(scrapy.Spider):
     name = 'ReadLightNovels'
     allowed_domains = ['readlightnovels.net']
     start_urls = ['https://readlightnovels.net/only-i-level-up.html']
+    chapter_url = 'https://readlightnovels.net/only-i-level-up/$.html'
+    page = 1
 
     def parse(self, response):
         for chapters in response.css('ul.list-chapter li'):
-            link = chapters.css("a::attr(href)").extract_first()
+            link = chapters.css('a::attr(href)').extract_first()
 
             yield response.follow(link, self.parse_chapter)
         
+        self.page += 1
+
+        next_page = response.css(f'ul.pagination-sm li a[data-page*="{self.page}"]::attr(href)').extract_first()
+        if next_page is not None:
+            yield response.follow(next_page, self.parse)
+
+        
     def parse_chapter(self, response):
         link = response.url
-        novel = response.css("a.truyen-title::text").extract()
-        title = response.css("a.chapter-title::text").extract()
-        text = response.css("div.chapter-content").extract()
+        novel = response.css('a.truyen-title::text').extract_first()
+        title = response.css('a.chapter-title::text').extract_first()
+        text = response.css('div.chapter-content').extract_first()
 
-        chapter = NovelChapterItem(novel = novel[0], title = title[0], link = link, text = text[0])
+        chapter = NovelChapterItem(novel = novel, title = title, link = link, text = text)
 
         yield chapter
